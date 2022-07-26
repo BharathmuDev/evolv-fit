04046815ffb6de16c62e1d453e211746b5a4a030
Bharathmogaveer <bharathmogaveer01@gmail.com>
2022-07-26 23:02:52 +0530

    add calories component

--- src/components/Dashboard/Calories/index.js
+++ src/components/Dashboard/Calories/index.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
 import "./index.css";
-
+import chartImage from "../../../assets/chartImage.png";
+import NegativeIcon from "../../Icons/NegativeIcon.js";
+import PlusIcon from "../../Icons/PlusIcon.js";
 
 const Calories = () => {
   return (
-    <div> </div>
-  )
-}
+    <div className="Calories__container">
+      <duv>
+        <img src={chartImage} alt="chart" height={"100px"} width={"100px"} />
+      </duv>
+      <div className="calories__info">
+        <div className="Icon">
+          <PlusIcon />
+        </div>
+        <div className="number">2.5K</div>
+        <div className="target">target</div>
+        <div className="Icon">
+          <NegativeIcon />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Calories
+export default Calories;
