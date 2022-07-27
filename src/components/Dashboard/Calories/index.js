f46ac02e400f4e36667be74ecd0af6f5d430dc5f
Bharathmogaveer <bharathmogaveer01@gmail.com>
2022-07-27 12:43:58 +0530

    add Increment/Decrement target to calories

--- src/components/Dashboard/Calories/index.js
+++ src/components/Dashboard/Calories/index.js
@@ -1,24 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 import chartImage from "../../../assets/chartImage.png";
 import NegativeIcon from "../../Icons/NegativeIcon.js";
 import PlusIcon from "../../Icons/PlusIcon.js";
 
 const Calories = () => {
+  const [calories, setCalories] = useState(2.5);
+
+  function incrementCalories() {
+    setCalories((prevCount) => prevCount + 0.5);
+  }
+
+  function decrementCalories() {
+    setCalories((prevCount) => prevCount - 0.5);
+  }
+
   return (
     <div className="Calories__container">
       <duv>
         <img src={chartImage} alt="chart" height={"100px"} width={"100px"} />
       </duv>
       <div className="calories__info">
-        <div className="Icon">
+        <button className="button" onClick={incrementCalories}>
           <PlusIcon />
-        </div>
-        <div className="number">2.5K</div>
+        </button>
+        <div className="number">{calories}K</div>
         <div className="target">target</div>
-        <div className="Icon">
+        <button className="button" onClick={decrementCalories}>
           <NegativeIcon />
-        </div>
+        </button>
       </div>
     </div>
   );
